2f6cae019ef979832a1b35f61d7c0f0e469c224e
heku <heku@sina.cn>
2021-07-31 22:10:16 +0800

    Add VS2022 support

--- publish.overview.md
+++ publish.overview.md
@@ -1,24 +1,40 @@
 Another open source Visual Studio extension that makes file/code comparison more easily.
 
+- [VS 2022](https://marketplace.visualstudio.com/items?itemName=Heku.VsDiff2022)
+- [VS 2015/2017/2019](https://marketplace.visualstudio.com/items?itemName=Heku.VsDiff)
+
 ## Features
-- Compare two selected files in Solution Explorer.
 
-    ![CompareSelectedFiles.png](Images/CompareSelectedFiles.png)
+- Compare two selected files in Solution Explorer (Folder View supported).
 
-- Compare the selected file with Clipboard content.
+    ![CompareSelectedFiles.png](Screenshots/CompareSelectedFiles.png)
 
-    ![CompareSelectedFileWithClipboard.png](Images/CompareSelectedFileWithClipboard.png)
+- Compare the selected file with Clipboard content (Folder View supported).
+
+    ![CompareSelectedFileWithClipboard.png](Screenshots/CompareSelectedFileWithClipboard.png)
 
 - Compare the selection code with Clipboard content.
 
-    ![CompareSelectionCodeWithClipboard.png](Images/CompareSelectionCodeWithClipboard.png)
+    ![CompareSelectionCodeWithClipboard.png](Screenshots/CompareSelectionCodeWithClipboard.png)
 
 - Compare the active document with Clipboard content.
 
-    ![CompareActiveDocumentWithClipboard.png](Images/CompareActiveDocumentWithClipboard.png)
+    ![CompareActiveDocumentWithClipboard.png](Screenshots/CompareActiveDocumentWithClipboard.png)
 
 ## Configurable
-![Configuration.png](Images/Configuration.png)
+
+![Configuration.png](Screenshots/Configuration.png)
+
+## Thanks
+
+Thanks for every rating, I'm glad the tool is helpful to you,
+because of a [network issue](https://github.com/heku/Kool.VsDiff/issues/5),
+I cannot reply to you directly in the marketplace.
+
+## Welcome
+
+I'm not a native English speaker, so if you could correct any grammer mistake, it would be very appreciated.
 
 ## Feedback
+
 If you have any question or problem, feel free to open an issue on [GitHub](https://github.com/heku/kool.vsdiff)
--- vsix.psm1
+++ vsix.psm1
@@ -3,10 +3,7 @@ $VsixCs = Resolve-Path ".\*\Vsix.cs"
 $VsixCsVersionPattern = 'VERSION = "([\d\\.]+)"'
 $VsixCsVersionFormat = 'VERSION = "{0}"'
 
-$SourceManifest = Resolve-Path ".\*\source.extension.vsixmanifest"
-$PublishManifest = ".\publish.manifest.json"
-$PublishVsixFile = Resolve-Path ".\*\bin\Release\*.vsix"
-
+$SourceManifests = Resolve-Path ".\*\source.extension.vsixmanifest"
 
 function UpdateVersion {
     [cmdletbinding()]
@@ -42,29 +39,32 @@ function UpdateManifestVersion {
         [string] $Version
     )
 
-    [xml]$content = Get-Content $SourceManifest
-    $content.PackageManifest.Metadata.Identity.Version = $Version
-    $content.Save($SourceManifest)
+    $SourceManifests | ForEach-Object {
+        [xml]$content = Get-Content $_
+        $content.PackageManifest.Metadata.Identity.Version = $Version
+        $content.Save($_)
 
-    Write-Host "Version updated:" $SourceManifest -ForegroundColor Green    
+        Write-Host "Version updated:" $_ -ForegroundColor Green   
+    } 
 }
 
 function PublishVsix {
     [cmdletbinding()]
     param(
-        [Parameter(Mandatory=$true)] 
-        [string] $PAT
+        [string] $PAT,
+        [string] $vsix,
+        [string] $manifest
     )
     
-    $installation = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -format json | ConvertFrom-Json
+    $installation = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -format json | ConvertFrom-Json
     $path = $installation.installationPath
     $vsixPublisher = Join-Path -Path $path -ChildPath "VSSDK\VisualStudioIntegration\Tools\Bin\VsixPublisher.exe" -Resolve
     Write-Host "VsixPublisher:" $vsixPublisher -ForegroundColor Green
     
-    Write-Host "Publish Manifest:" $PublishManifest -ForegroundColor Green
-    Write-Host "Vsix:" $PublishVsixFile -ForegroundColor Green
+    Write-Host "Publish Manifest:" $manifest -ForegroundColor Green
+    Write-Host "Vsix:" $vsix -ForegroundColor Green
 
-    & $vsixPublisher publish -payload $PublishVsixFile -publishManifest $PublishManifest -personalAccessToken $PAT -ignoreWarnings "VSIXValidatorWarning01,VSIXValidatorWarning02,VSIXValidatorWarning08"
+    & $vsixPublisher publish -payload $vsix -publishManifest $manifest -personalAccessToken $PAT
 }
 
 Export-ModuleMember -Function UpdateVersion, PublishVsix
