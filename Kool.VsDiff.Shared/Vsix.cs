522fcc84125639e1590f7fbc00f30a7e398aa378
heku <heku.git@outlook.com>
2022-03-12 16:36:18 +0800

    Cleanup

--- Kool.VsDiff.Shared/Vsix.cs
+++ Kool.VsDiff.Shared/Vsix.cs
@@ -7,8 +7,3 @@ internal static class Vsix
     public const string PACKAGE = "VS Diff";
     public const string GITHUB = "https://github.com/heku/kool.vsdiff";
 }
-
-// https://github.com/madskristensen/ImageOptimizer/commit/30ae022f54d57eec6d799b7ba5ea22e3b6effb39
-
-// https://docs.microsoft.com/en-us/visualstudio/extensibility/migration/update-visual-studio-extension?view=vs-2022#modernize-your-vsix-project
-// https://docs.microsoft.com/en-us/visualstudio/extensibility/migration/samples?view=vs-2022
